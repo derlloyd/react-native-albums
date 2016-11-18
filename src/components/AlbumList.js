a4923d0694f456c7f6bbb73fc530ff0cbc9b353e
Derek Lloyd <dereklloyd@Dereks-MacBook-Pro.local>
2016-11-18 16:16:34 -0500

    refactor AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>ALBUMS LIST!!!!!!</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>ALBUMS MY LIST!!!!!!</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
