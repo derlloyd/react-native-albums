fe61b48a6bd96e20985f09ae89a83bff14130091
Derek Lloyd <dereklloyd@Dereks-MacBook-Pro.local>
2016-11-18 21:57:19 -0500

    Card header styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    // instead of props as argument, use destructured album (props.album)
+    const { title, artist, thumbnail_image } = album;
+    // optionally could also destructure styles object
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <View>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50, // need to specify or will not show
+        width: 50
+    }
 };
 
 export default AlbumDetail;
